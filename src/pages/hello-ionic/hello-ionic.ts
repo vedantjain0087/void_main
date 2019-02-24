d1b576c94695993dd77887ee730df74ac27a8f02
Vedant <mainvedant.vns123@gmail.com>
2019-02-24 10:12:47 +0530

    Database

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { MymapsPage } from '../mymaps/mymaps';
+import { HTTP } from '@ionic-native/http';
+
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private http: HTTP) {
+    this.fetch_api();
+  }
+  async fetch_api(){
+    this.http.post('', {}, {})
+    .then(data => {
+  
+      console.log(data.status);
+      console.log(data.data); // data received by server
+      console.log(data.headers);
+  
+    })
+    .catch(error => {
+  
+      console.log(error.status);
+      console.log(error.error); // error message as string
+      console.log(error.headers);
+  
+    });
   }
 
   navi(){
